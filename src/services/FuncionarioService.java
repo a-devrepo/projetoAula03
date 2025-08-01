cd3c8e9101d490c5bb52eb19aec145e03582750d
alison <adevpr11@gmail.com>
2025-08-01 16:18:18 -0300

    feat: adiciona FuncionarioService

---
+++ src/services/FuncionarioService.java
@@ -0,0 +1,16 @@
+package services;
+import entities.Funcionario;
+import repositories.FuncionarioRepository;
+
+public class FuncionarioService {
+
+	private final FuncionarioRepository repository;
+	
+	public FuncionarioService(FuncionarioRepository repository) {
+		this.repository = repository;
+	}
+	
+	public void cadastrarFuncionario(Funcionario funcionario) {
+		repository.inserir(funcionario);
+	}
+}
