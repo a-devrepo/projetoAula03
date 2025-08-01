cb5788a6d07ad086106fc7606cebd37a929d2e4a
alison <adevpr11@gmail.com>
2025-08-01 10:52:31 -0300

    feat: adiciona coleta de dados de endereço

--- src/controllers/FuncionarioController.java
+++ src/controllers/FuncionarioController.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.UUID;
 
+import entities.Endereco;
 import entities.Funcionario;
 
 public class FuncionarioController {
@@ -19,16 +20,46 @@ public class FuncionarioController {
 		
 		funcionario.setId(UUID.randomUUID());
 		
-		System.out.print("INFORME O NOME:............................: ");
+		System.out.print("INFORME O NOME.............................: ");
 		funcionario.setNome(scanner.nextLine());
 		
-		System.out.print("INFORME O CPF.:............................: ");
+		System.out.print("INFORME O CPF:.............................: ");
 		funcionario.setCpf(scanner.nextLine());
 		
 		System.out.print("DATA DE ADMISSÃO (dd/MM/yyyy)..............: ");
 		var dataAdmissaoInput = scanner.nextLine();
 		var dataAdmissao = LocalDate.parse(dataAdmissaoInput,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		funcionario.setDataAdmissao(dataAdmissao);
+		
+		var endereco = new Endereco();
+		
+		endereco.setId(UUID.randomUUID());
+		
+		System.out.print("INFORME O LOGRADOURO.......................: ");
+		endereco.setLogradouro(scanner.nextLine());
+		
+		System.out.print("INFORME O NÚMERO...........................: ");
+		endereco.setNumero(scanner.nextLine());
+		
+		System.out.print("INFORME O COMPLEMENTO......................: ");
+		endereco.setComplemento(scanner.nextLine());
+		
+		System.out.print("INFORME O BAIRRO............................: ");
+		endereco.setBairro(scanner.nextLine());
+		
+		System.out.print("INFORME A CIDADE............................: ");
+		endereco.setCidade(scanner.nextLine());
+		
+		System.out.print("INFORME O ESTADO............................: ");
+		endereco.setEstado(scanner.nextLine());
+		
+		System.out.print("INFORME O CEP...............................: ");
+		endereco.setCep(scanner.nextLine());
+		
+		
 		
 		scanner.close();
+		
+		System.out.println("\nCADASTRO REALIZADO COM SUCESSO!\n");
 	}
 }
