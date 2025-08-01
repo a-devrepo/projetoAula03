38ebf512de59ca4e906433db1c4fd0dcff94969e
alison <adevpr11@gmail.com>
2025-08-01 16:30:51 -0300

    chore: configura instâncias e a integração entre as camadas do projeto

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,12 +1,18 @@
 package principal;
 
 import controllers.FuncionarioController;
+import factories.ConnectionFactory;
+import repositories.FuncionarioRepository;
+import services.FuncionarioService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		var funcionarioController = new FuncionarioController();
+		var connectionFactory = new ConnectionFactory();
+		var funcionarioRepository = new FuncionarioRepository(connectionFactory);
+		var funcionarioService = new FuncionarioService(funcionarioRepository);
+		var funcionarioController = new FuncionarioController(funcionarioService);
 		
 		funcionarioController.cadastrarFuncionario();
 		funcionarioController.fecharScanner();
