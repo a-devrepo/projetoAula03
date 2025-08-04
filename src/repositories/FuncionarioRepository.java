e1bff35d61e10be31b2f05a46da4d994a5d5b355
alison <adevpr11@gmail.com>
2025-08-04 12:15:53 -0300

    feat: insere também dados de endereço ao cadastrar funcionário

--- src/repositories/FuncionarioRepository.java
+++ src/repositories/FuncionarioRepository.java
@@ -15,16 +15,36 @@ public class FuncionarioRepository {
 	}
 
 	public void inserir(Funcionario funcionario) {
-		var sql = "INSERT INTO funcionario (id_funcionario, nome, cpf, data_admissao) values (?,?,?,?)";
-
+		
+		var sqlFuncionario = "INSERT INTO funcionario (id_funcionario, nome, cpf, data_admissao) values (?,?,?,?)";
+		var sqlEndereco = """
+			    INSERT INTO endereco 
+			    (id_endereco, logradouro, numero, complemento, bairro, cidade, estado, cep, id_funcionario) 
+			    VALUES (?,?,?,?,?,?,?,?,?)
+			    """;
 		try (var connection = connectionFactory.getConnection(); 
-				var statement = connection.prepareStatement(sql)) {
+				var statementFuncionario = connection.prepareStatement(sqlFuncionario);
+				var statementEndereco = connection.prepareStatement(sqlEndereco)) {
+			
 
-			statement.setString(1, funcionario.getId().toString());
-			statement.setString(2, funcionario.getNome());
-			statement.setString(3, funcionario.getCpf());
-			statement.setDate(4, java.sql.Date.valueOf(funcionario.getDataAdmissao()));
-			statement.executeUpdate();
+			statementFuncionario.setString(1, funcionario.getId().toString());
+			statementFuncionario.setString(2, funcionario.getNome());
+			statementFuncionario.setString(3, funcionario.getCpf());
+			statementFuncionario.setDate(4, java.sql.Date.valueOf(funcionario.getDataAdmissao()));
+			statementFuncionario.executeUpdate();
+			
+			statementEndereco.setString(1, funcionario.getEndereco().getId().toString());
+			statementEndereco.setString(2, funcionario.getEndereco().getLogradouro());
+			statementEndereco.setString(3, funcionario.getEndereco().getNumero());
+			statementEndereco.setString(4, funcionario.getEndereco().getComplemento());
+			statementEndereco.setString(5, funcionario.getEndereco().getBairro());
+			statementEndereco.setString(6, funcionario.getEndereco().getCidade());
+			statementEndereco.setString(7, funcionario.getEndereco().getEstado());
+			statementEndereco.setString(8, funcionario.getEndereco().getCep());
+			statementEndereco.setString(9, funcionario.getId().toString());
+			statementEndereco.executeUpdate();
+			
+
 		} catch (SQLException sqlException) {
 			throw new RepositoryException("Erro ao inserir Funcionário: ",sqlException);
 		}
