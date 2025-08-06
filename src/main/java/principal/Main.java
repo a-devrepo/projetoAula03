dabb72199c40ca54840546857fbb7d5fe99a27ff
alison <adevpr11@gmail.com>
2025-08-06 20:45:49 -0300

    refactor: melhora tratamento de erros e liberação de recursos no método main

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -15,17 +15,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		var dataBaseConfig = new PropertiesDataBaseConfigLoader().loadDatabaseConfig();
-		var connectionFactory = new ConnectionFactory(dataBaseConfig);
-		var funcionarioRepository = new FuncionarioRepositoryImpl(connectionFactory);
-		var funcionarioService = new FuncionarioServiceImpl(funcionarioRepository);
 		var consoleReaderWriter = new ConsoleReaderWriter(new Scanner(System.in));
 		var errorHandler = new ErrorHandler(consoleReaderWriter);
-		var funcionarioFormReader = new FuncionarioFormReader(consoleReaderWriter);
-		var funcionarioController = new FuncionarioController(funcionarioService, errorHandler, consoleReaderWriter,
-				funcionarioFormReader);
+		
+		try {
+			var dataBaseConfig = new PropertiesDataBaseConfigLoader().loadDatabaseConfig();
+			var connectionFactory = new ConnectionFactory(dataBaseConfig);
+			var funcionarioRepository = new FuncionarioRepositoryImpl(connectionFactory);
+			var funcionarioService = new FuncionarioServiceImpl(funcionarioRepository);
+			var funcionarioFormReader = new FuncionarioFormReader(consoleReaderWriter);
+			var funcionarioController = new FuncionarioController(funcionarioService, 
+					errorHandler, consoleReaderWriter,funcionarioFormReader);
 
-		funcionarioController.cadastrarFuncionario();
-		consoleReaderWriter.fecharScanner();
+			funcionarioController.cadastrarFuncionario();
+
+		} catch (Exception exception) {
+			consoleReaderWriter.exibirMensagemErro(exception.getMessage());
+			consoleReaderWriter.exibirDetalhesErro(exception);
+		} finally {
+			consoleReaderWriter.fecharScanner();
+		}
 	}
 }
